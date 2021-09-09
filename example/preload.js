3a7763f54a18c5ef47205df687d01385567a66de
Colin Mitchell <colin@muffinlabs.com>
2021-09-09 17:23:25 -0400

    Update node-abi and bindings
    - Update node-abi, bindings, and a few other modules
    - Update electron to v14
    - Update example app to use IPC and contextBridge

---
+++ example/preload.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const {
+  contextBridge, ipcRenderer
+} = require('electron');
+contextBridge.exposeInMainWorld(
+  'forcefocus',
+  {
+    focusWindow: () => {
+      return ipcRenderer.invoke('force-focus');
+    }
+  }
+);
